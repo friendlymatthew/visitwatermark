52c383375658726851e4f9d9b6890ee9e8d8b498
matthew kim <friendlymatthew@berkeley.edu>
2023-02-04 11:45:37 -0500

    removed css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+
 import Availability from "@/components/Availability";
-import { AnimationOnScroll } from "react-animation-on-scroll";
+
 import Amenities from "@/components/Amenities";
 import Footer from "@/components/Footer";
 import Rating from "@/components/Rating";
